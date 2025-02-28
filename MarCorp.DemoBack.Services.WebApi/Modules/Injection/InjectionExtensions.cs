f920382908cf745f59b45c1ddef6f3479cbe8461
Daniel Mar <daniel.mar@iest.edu.mx>
2025-02-28 00:11:05 -0600

    Refactor dependency injection in AddInjection method
    Removed direct singleton registration of DapperContext.
    Retained registration of IDapperContext to promote better
    abstraction and testability in the code.

--- MarCorp.DemoBack.Services.WebApi/Modules/Injection/InjectionExtensions.cs
+++ MarCorp.DemoBack.Services.WebApi/Modules/Injection/InjectionExtensions.cs
@@ -24,7 +24,6 @@ namespace MarCorp.DemoBack.Services.WebApi.Modules.Injection
         public static IServiceCollection AddInjection(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddSingleton<IConfiguration>(configuration);
-            //services.AddSingleton<DapperContext>();
             services.AddSingleton<IDapperContext, DapperContext>();
             services.AddScoped<ICustomerApplication, CustomerApplication>();
             services.AddScoped<ICustomerDomain, CustomerDomain>();
